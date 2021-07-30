34695be03e174dedf88584f94385274846afc028
JonesLLL <87405335+JonesLLL@users.noreply.github.com>
2021-07-30 17:19:34 +0800

    these place is for notes

--- jellyfish_test1/Release/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\cslaw\AppData\Local\Temp\VMBuilds\jellyfish_test1\mega_atmega2560\Release\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\azucglgt.vok\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
